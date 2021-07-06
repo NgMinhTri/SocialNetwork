65bce82fe37ab2bd2dbbbc316d9d2e6e86fb8c34
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-07-06 10:43:25 +0700

    Fix chage-pass Admin

--- admin/change-password.php
+++ admin/change-password.php
@@ -15,14 +15,14 @@
                             <h3 class="mb-0">Thay đổi mật khẩu</h3>
                         </div>
                         <div class="card-body">
-                            <form class="form" id="change-password" >
+                            <form class="form" id="passwordForm" >
                                 <!-- <div class="form-group">
                                     <label for="inputPasswordOld">Mật khẩu hiện tại</label>
                                     <input type="password" class="form-control" id="inputPasswordOld" required="">
                                 </div> -->
                                 <div class="form-group">
                                     <label for="inputPasswordNew">Mật khẩu mới</label>
-                                    <input type="password" name="password" class="form-control" id="inputPasswordNew" required="">
+                                    <input type="password" name="password" class="form-control" id="inputPasswordNew" minlength="8" required="">
                                     <span class="form-text small text-muted">
                                             Mật khẩu phải từ 8-20 kí tự và <em>không</em> chứa khoảng trắng!
                                         </span>
@@ -42,56 +42,64 @@
 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script type="text/javascript">
-$(document).ready(function(){  
+$(document).ready(function() {
 
-    function getCookie(cname){
+    $(document).on('submit', '#passwordForm', function() {
+
+        var changePassword = $(this);
+        var jwt = getCookie('jwt');
+        var updatePassAdmin = changePassword.serializeObject()
+
+        updatePassAdmin.jwt = jwt;
+
+        var form_data = JSON.stringify(updatePassAdmin);
+        $.ajax({
+            url: "../api/admin/change-password.php",
+            type: "POST",
+            contentType: 'application/json',
+            data: form_data,
+            success: function(result) {
+
+                $('#response').html(
+                    "<div class='alert alert-success'>Mật khẩu đã được cập nhật.</div>");
+                changePassword.find('input').val('');
+            },
+            error: function(xhr, resp, text) {
+                $('#response').html(
+                    "<div class='alert alert-danger'>Mật khẩu phải từ 8-20 kí tự và không chứa khoảng trắng!.</div>");
+            }
+        });
+
+        return false;
+    });
+
+    // function to set cookie
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
         var name = cname + "=";
         var decodedCookie = decodeURIComponent(document.cookie);
         var ca = decodedCookie.split(';');
-        for(var i = 0; i <ca.length; i++) {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
-            while (c.charAt(0) == ' '){
+            while (c.charAt(0) == ' ') {
                 c = c.substring(1);
             }
-     
+
             if (c.indexOf(name) == 0) {
                 return c.substring(name.length, c.length);
             }
         }
         return "";
     }
+    // submit form data to api
+    $.fn.serializeObject = function() {
 
-    $(document).on('submit', '#change-password', function(){    
-
-        var change_password_form=$(this);
-        var jwt = getCookie('jwt');
-        var change_password_form_obj = change_password_form.serializeObject();
-         change_password_form_obj.jwt = jwt;
-        
-        var form_data = JSON.stringify(change_password_form_obj);
-        
-        $.ajax({
-            url: "../api/admin/change-password.php",
-            type : "POST",
-            contentType : 'application/json',
-            data : form_data,
-            success : function(result){  
-
-            $('#response').html("<div class='alert alert-success'>Thêm danh mục câu hỏi thành công.</div>");
-            
-                          
-            }
-            // lỗi nếu dăng nhập không thành công
-            error: function(xhr, resp, text){
-            $('#response').html("<div class='alert alert-danger'>Thêm danh mục câu hỏi thất bại. Vui lòng thử lại.</div>");
-            }
-        });
-        return false;
-    });
-     
-    // hàm biến giá trị form thành dạng json
-    $.fn.serializeObject = function(){
-     
         var o = {};
         var a = this.serializeArray();
         $.each(a, function() {
@@ -106,6 +114,6 @@ $(document).ready(function(){
         });
         return o;
     };
-});   
+});  
 </script>
           
